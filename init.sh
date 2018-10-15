056ea23f173f37bea747cbbd5ba0bc15841f5102
HomMarkHunt <honmaso@gmail.com>
2018-10-15 19:40:03 +0900

    fix: fixed error when dir is not created

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+mkdir -p ~/.config/brewfile
+mkdir -p ~/.config/fish
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/Brewfile ~/.config/brewfile/Brewfile
 ln -sf ~/dotfiles/config.fish ~/.config/fish/config.fish
