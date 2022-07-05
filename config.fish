f5706aa01eae8341a6a99328744a7522655fe27e
HomMarkHunt <honmaso@gmail.com>
2022-07-06 00:51:30 +0900

    update

--- config.fish
+++ config.fish
@@ -1,1 +1,5 @@
+set -Ux fish_user_paths $HOME/.rbenv/bin $fish_user_paths
+set -Ux fish_user_paths $HOME/Library/Python/3.8/bin $fish_user_paths
 
+eval (/opt/homebrew/bin/brew shellenv)
+source (rbenv init - | psub)
--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p ~/.config/brewfile
 mkdir -p ~/.config/fish
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/Brewfile ~/.config/brewfile/Brewfile
-ln -sf ~/dotfiles/config.fish ~/.config/fish/config.fish
+ln -sf ~/github/HomMarkHunt/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/github/HomMarkHunt/dotfiles/Brewfile ~/.config/brewfile/Brewfile
+ln -sf ~/github/HomMarkHunt/dotfiles/config.fish ~/.config/fish/config.fish
