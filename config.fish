c89052c29fb27c719ad8192a40163f1b4ef54e5f
HomMarkHunt <honmaso@gmail.com>
2025-08-30 15:02:13 +0900

    update

--- config.fish
+++ config.fish
@@ -1,5 +1,7 @@
 set -Ux fish_user_paths $HOME/.rbenv/bin $fish_user_paths
 set -Ux fish_user_paths $HOME/Library/Python/3.8/bin $fish_user_paths
+set -x GOPATH $HOME/go
+set -x PATH $PATH $GOPATH/bin ~/HomMarkHunt/bin
 
 eval (/opt/homebrew/bin/brew shellenv)
 source (rbenv init - | psub)
